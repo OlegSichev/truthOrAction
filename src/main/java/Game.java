70fbf53587305dc490a8b4c52a955f55f7668909
Oleg Sichev <sichev.oleg@bk.ru>
2023-05-30 18:49:45 +0300

    Serialization and Deserialization work

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,62 +1,43 @@
-import java.io.Serializable;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;;
 
 public class Game implements Serializable {
-    private static final long serialVersionUID = 1L; // версия программя для сериализации
-    // TODO заполнить ArrayList'ы правдой и действием, проверить работоспособность программы
+    private static final long serialVersionUID = 1L;
     static Scanner scanner = new Scanner(System.in);
     static Random random = new Random();
     static int countTrueMan = 0;
     static int countTrueWoman = 0;
     static List<String> easyTruthMan = new ArrayList<>();
-
     static List<String> easyTruthWoman = new ArrayList<>();
     static List<String> normalTruthMan = new ArrayList<>();
     static List<String> normalTruthWoman = new ArrayList<>();
     static List<String> hardTruthMan = new ArrayList<>();
     static List<String> hardTruthWoman = new ArrayList<>();
-
     static List<String> easyActionMan = new ArrayList<>();
     static List<String> easyActionWoman = new ArrayList<>();
     static List<String> normalActionMan = new ArrayList<>();
     static List<String> normalActionWoman = new ArrayList<>();
     static List<String> hardActionMan = new ArrayList<>();
     static List<String> hardActionWoman = new ArrayList<>();
-
     static List<String> truthMan;
     static List<String> truthWoman;
     static List<String> actionMan;
     static List<String> actionWoman;
 
-    static void serialization(){
-        List<List>listsSerialization = new ArrayList<>();
-        //todo
-        listsSerialization.add(easyTruthMan);
-        listsSerialization.add(easyTruthWoman);
-        listsSerialization.add(easyActionMan);
-        listsSerialization.add(easyActionWoman);
-        listsSerialization.add(normalTruthMan);
-        listsSerialization.add(normalTruthWoman);
-        listsSerialization.add(normalActionMan);
-        listsSerialization.add(normalActionWoman);
-        listsSerialization.add(hardTruthMan);
-        listsSerialization.add(hardTruthWoman);
-        listsSerialization.add(hardActionMan);
-        listsSerialization.add(hardActionWoman);
-    }
-
-    static void createLists(){
-        //TODO написать логику для создания только тех листов, которые нам нужны для выбранного Левела (если лень, можно и все создавать)
+    static void createLists() {
         easyActionMan.add("Пробное легкое действие");
         easyActionMan.add("Пробное легкое действие2");
     }
 
-    public static void start(Main.Level level){
+    public static void start(Main.Level level) {
         createLists();
-        if (level.equals(Main.Level.easy)){
+        if (level.equals(Main.Level.easy)) {
             truthMan = easyTruthMan;
             truthWoman = easyTruthWoman;
             actionMan = easyActionMan;
@@ -82,16 +63,13 @@ public class Game implements Serializable {
                     System.out.println(actionMan.get(random.nextInt(actionMan.size())));
                 }
                 countTrueMan++;
-                System.out.println(truthMan.get(random.nextInt(truthMan.size())));
-                //вызывается лист с рандомным вопросом правды
+                System.out.println(truthMan.get(random.nextInt(truthMan.size()))); //вызывается лист с рандомным вопросом правды
             } else if (inputMan == 2) {
-                System.out.println(actionMan.get(random.nextInt(actionMan.size())));
-                //вызывается лист с рандомным действием
+                System.out.println(actionMan.get(random.nextInt(actionMan.size()))); //вызывается лист с рандомным действием
             } else if (inputMan == -1) {
                 System.exit(0);
             } else {
-                System.out.println("Вы ввели некорректное число");
-               // start();
+                System.out.println("Вы ввели некорректное число"); // start();
             }
             System.out.println("Нажмите Enter для продолжения");
             scanner.nextLine();
@@ -104,17 +82,68 @@ public class Game implements Serializable {
                     System.out.println(actionWoman.get(random.nextInt(actionWoman.size())));
                 }
                 countTrueMan++;
-                System.out.println(truthWoman.get(random.nextInt(truthWoman.size())));
-                //вызывается лист с рандомным вопросом правды
+                System.out.println(truthWoman.get(random.nextInt(truthWoman.size()))); //вызывается лист с рандомным вопросом правды
             } else if (inputMan == 2) {
-                System.out.println(actionWoman.get(random.nextInt(actionWoman.size())));
-                //вызывается лист с рандомным действием
+                System.out.println(actionWoman.get(random.nextInt(actionWoman.size()))); //вызывается лист с рандомным действием
             } else if (inputWoman == -1) {
                 System.exit(0);
             } else {
                 System.out.println("Вы ввели некорректное число");
                 start(level);
             }
+        }
+    }
+
+    public static void serializeLists() {
+        try {
+            FileOutputStream fileOut =
+                    new FileOutputStream("data.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(easyTruthMan);
+            out.writeObject(easyTruthWoman);
+            out.writeObject(normalTruthMan);
+            out.writeObject(normalTruthWoman);
+            out.writeObject(hardTruthMan);
+            out.writeObject(hardTruthWoman);
+            out.writeObject(easyActionMan);
+            out.writeObject(easyActionWoman);
+            out.writeObject(normalActionMan);
+            out.writeObject(normalActionWoman);
+            out.writeObject(hardActionMan);
+            out.writeObject(hardActionWoman);
+            out.close();
+            fileOut.close();
+            System.out.println("Serialized data is saved in data.ser");
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+
+    public static void deserializeLists() {
+        try {
+            FileInputStream fileIn = new FileInputStream("data.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            easyTruthMan = (List<String>) in.readObject();
+            easyTruthWoman = (List<String>) in.readObject();
+            normalTruthMan = (List<String>) in.readObject();
+            normalTruthWoman = (List<String>) in.readObject();
+            hardTruthMan = (List<String>) in.readObject();
+            hardTruthWoman = (List<String>) in.readObject();
+            easyActionMan = (List<String>) in.readObject();
+            easyActionWoman = (List<String>) in.readObject();
+            normalActionMan = (List<String>) in.readObject();
+            normalActionWoman = (List<String>) in.readObject();
+            hardActionMan = (List<String>) in.readObject();
+            hardActionWoman = (List<String>) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+            return;
+        } catch (ClassNotFoundException c) {
+            System.out.println("List class not found");
+            c.printStackTrace();
+            return;
         }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -40,67 +40,83 @@ public class Main {
             } else if (input == -1) {
                 System.exit(0);
             } else if (input == 900) {
-                System.out.println("Выбран режим разработчика\nВыберите пункт меню цифрой:\n1. Редактирование легкого" +
-                        " режима\n2. Редактирование среднего режима\n3. Редактирование тяжелого режима");
-                int correctInput = scanInt.nextInt();
-                if (correctInput == 1){
-                    System.out.println("1.Добавить правду в легкий режим для девушки\n2. Добавить правду в легкий" +
-                            " режим для парня\n3. Добавить действие в легкий режим для девушки\n4. Добавить действие" +
-                            " в легкий режим для парня");
-                    correctInput = scanInt.nextInt();
-                    if (correctInput == 1){
-                        System.out.println("Введите правду в легкий режим для девушки:");
-                        //String easyTruthWoman = scanStr.nextLine(); если не будет работать, то использовать
-                        // эту переменную
-                        Game.easyTruthWoman.add(scanStr.nextLine());
-                    } else if (correctInput == 2){
-                        System.out.println("Введите правду в легкий режим для парня:");
-                        Game.easyTruthMan.add(scanStr.nextLine());
-                    } else if (correctInput == 3) {
-                        System.out.println("Введите действие в легкий режим для девушки:");
-                        Game.easyActionWoman.add(scanStr.nextLine());
-                    } else if (correctInput == 4) {
-                        System.out.println("Введите действие в легкий режим для парня:");
-                        Game.easyActionMan.add(scanStr.nextLine());
-                    }
-                } else if (correctInput == 2) {
-                    System.out.println("1.Добавить правду в средний режим для девушки\n2. Добавить правду в средний " +
-                            "режим для парня\n3. Добавить действие в средний режим для девушки\n4. Добавить действие в средний режим для парня");
-                    correctInput = scanInt.nextInt();
-                    if (correctInput == 1){
-                        System.out.println("Введите правду в средний режим для девушки:");
-                        Game.normalTruthWoman.add(scanStr.nextLine());
-                    } else if (correctInput == 2){
-                        System.out.println("Введите правду в средний режим для парня:");
-                        Game.normalTruthMan.add(scanStr.nextLine());
-                    } else if (correctInput == 3) {
-                        System.out.println("Введите действие в средний режим для девушки:");
-                        Game.normalActionWoman.add(scanStr.nextLine());
-                    } else if (correctInput == 4) {
-                        System.out.println("Введите действие в средний режим для парня:");
-                        Game.normalActionMan.add(scanStr.nextLine());
-                    }
-                } else if (correctInput == 3) {
-                    System.out.println("1.Добавить правду в тяжелый режим для девушки\n2. Добавить правду в тяжелый " +
-                            "режим для парня\n3. Добавить действие в тяжелый режим для девушки\n4. Добавить" +
-                            " действие в тяжелый режим для парня");
-                    correctInput = scanInt.nextInt();
-                    if (correctInput == 1){
-                        System.out.println("Введите правду в тяжелый режим для девушки:");
-                        Game.hardTruthWoman.add(scanStr.nextLine());
-                    } else if (correctInput == 2){
-                        System.out.println("Введите правду в тяжелый режим для парня:");
-                        Game.hardTruthMan.add(scanStr.nextLine());
-                    } else if (correctInput == 3) {
-                        System.out.println("Введите действие в тяжелый режим для девушки:");
-                        Game.hardActionWoman.add(scanStr.nextLine());
-                    } else if (correctInput == 4) {
-                        System.out.println("Введите действие в тяжелый режим для парня:");
-                        Game.hardActionMan.add(scanStr.nextLine());
-                    }
-                }
+                developerMode();
             } else {
                 System.out.println("Такой цифры не существует, попробуем еще раз :)");
+            }
+        }
+    }
+
+    public static void developerMode(){
+        while (true){
+            System.out.println("Выбран режим разработчика\nВыберите пункт меню цифрой:\n1. Редактирование легкого" +
+                    " режима\n2. Редактирование среднего режима\n3. Редактирование тяжелого режима\n4. Сериализация\n5. Десериализация\n-1 для выхода в главное меню\n-2 для завершения программы");
+            int correctInput = scanInt.nextInt();
+            if (correctInput == 1){
+                System.out.println("1.Добавить правду в легкий режим для девушки\n2. Добавить правду в легкий" +
+                        " режим для парня\n3. Добавить действие в легкий режим для девушки\n4. Добавить действие" +
+                        " в легкий режим для парня");
+                correctInput = scanInt.nextInt();
+                if (correctInput == 1){
+                    System.out.println("Введите правду в легкий режим для девушки:");
+                    //String easyTruthWoman = scanStr.nextLine(); если не будет работать, то использовать
+                    // эту переменную
+                    Game.easyTruthWoman.add(scanStr.nextLine());
+                } else if (correctInput == 2){
+                    System.out.println("Введите правду в легкий режим для парня:");
+                    Game.easyTruthMan.add(scanStr.nextLine());
+                } else if (correctInput == 3) {
+                    System.out.println("Введите действие в легкий режим для девушки:");
+                    Game.easyActionWoman.add(scanStr.nextLine());
+                } else if (correctInput == 4) {
+                    System.out.println("Введите действие в легкий режим для парня:");
+                    Game.easyActionMan.add(scanStr.nextLine());
+                }
+            } else if (correctInput == 2) {
+                System.out.println("1.Добавить правду в средний режим для девушки\n2. Добавить правду в средний " +
+                        "режим для парня\n3. Добавить действие в средний режим для девушки\n4. Добавить действие в средний режим для парня");
+                correctInput = scanInt.nextInt();
+                if (correctInput == 1){
+                    System.out.println("Введите правду в средний режим для девушки:");
+                    Game.normalTruthWoman.add(scanStr.nextLine());
+                } else if (correctInput == 2){
+                    System.out.println("Введите правду в средний режим для парня:");
+                    Game.normalTruthMan.add(scanStr.nextLine());
+                } else if (correctInput == 3) {
+                    System.out.println("Введите действие в средний режим для девушки:");
+                    Game.normalActionWoman.add(scanStr.nextLine());
+                } else if (correctInput == 4) {
+                    System.out.println("Введите действие в средний режим для парня:");
+                    Game.normalActionMan.add(scanStr.nextLine());
+                }
+            } else if (correctInput == 3) {
+                System.out.println("1.Добавить правду в тяжелый режим для девушки\n2. Добавить правду в тяжелый " +
+                        "режим для парня\n3. Добавить действие в тяжелый режим для девушки\n4. Добавить" +
+                        " действие в тяжелый режим для парня");
+                correctInput = scanInt.nextInt();
+                if (correctInput == 1) {
+                    System.out.println("Введите правду в тяжелый режим для девушки:");
+                    Game.hardTruthWoman.add(scanStr.nextLine());
+                } else if (correctInput == 2) {
+                    System.out.println("Введите правду в тяжелый режим для парня:");
+                    Game.hardTruthMan.add(scanStr.nextLine());
+                } else if (correctInput == 3) {
+                    System.out.println("Введите действие в тяжелый режим для девушки:");
+                    Game.hardActionWoman.add(scanStr.nextLine());
+                } else if (correctInput == 4) {
+                    System.out.println("Введите действие в тяжелый режим для парня:");
+                    Game.hardActionMan.add(scanStr.nextLine());
+                }
+            } else if (correctInput == 4) {
+                Game.serializeLists();
+            } else if (correctInput == 5) {
+                Game.deserializeLists();
+            } else if (correctInput == -1) {
+                menu();
+                //Game.serialize();
+            } else if (correctInput == -2) {
+                //Game.serialize();
+                System.exit(0);
             }
         }
     }
